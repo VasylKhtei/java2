e14036c71d8a170f8f2ce11949b741765c66e172
Vasyl Khtei <vasylkhtei@gmail.com>
2019-03-07 18:26:39 +0200

    final ver for mvn bild

---
+++ src/test/java/hello/AppTest.java
@@ -0,0 +1,31 @@
+package hello;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+import org.junit.Assert;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+   @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+    
+    @Test
+    public void testNothing() {
+    }
+    
+  /**   @Test
+    public void testWillAlwaysFail() {
+        Assert.fail("An error message");
+    }
+    */
+}
